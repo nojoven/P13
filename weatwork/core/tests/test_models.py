fd67fe4a828e336500e4d9eac80cac3896a1072e
Joseph <work.cedricjoseph@gmail.com>
2021-10-07 17:43:05 +0200

    feat: create superuser

--- weatwork/core/tests/test_models.py
+++ weatwork/core/tests/test_models.py
@@ -28,5 +28,16 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 123)
+            
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user =get_user_model().objects.create_superuser(
+            'test@maintainerdev.com', 
+            'test123'
+        )
+        self.assertTrue(user.is_superuser) # is_superuser() comes from PermissionsMixin
+        self.assertTrue(user.is_staff)
+        
+        
         
         
