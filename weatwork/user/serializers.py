3666f8044aedb54d3a6140629ecd77c7e24bded8
Joseph <work.cedricjoseph@gmail.com>
2021-10-21 12:09:37 +0200

    feat: add create token api with its view

--- weatwork/user/serializers.py
+++ weatwork/user/serializers.py
@@ -1,11 +1,10 @@
-from django.contrib.auth import get_user_model
-
+from django.contrib.auth import get_user_model, authenticate
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
 class  UserSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
-    
     class Meta:
         model = get_user_model()
         fields = ('email', 'password', 'name')
@@ -16,3 +15,30 @@ class  UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Creates ans returns a new user with encrypted password"""
         return get_user_model().objects.create_user(**validated_data)
+
+
+class  AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False, # Because by default it is set to True  
+    )
+    
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        
+        if not user:
+            msg = _('Unable to authenticate with the provided credentials.')
+            raise serializers.ValidationError(msg, code='authentication')
+            
+        attrs['user'] = user
+        return attrs
