26385b902b6187d4cac3f68ecee54dc95be59b77
Joseph <work.cedricjoseph@gmail.com>
2021-10-26 14:18:07 +0200

    feat: add home view and template

--- weatwork/core/urls.py
+++ weatwork/core/urls.py
@@ -7,6 +7,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('home/', views.index, name='index'),
-    # path('home/', views.CreateUserView.as_view(), name='create'),
+    path('users/', views.users, name='users'),
     # path('me/', views.ManageUserView.as_view(), name='me'),
 ]
--- weatwork/core/views.py
+++ weatwork/core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
+from  django.template import loader
 
+def users(request):
+    users_list = User.objects.all()
+    template = loader.get_template('core/index.html')
+    context = {
+        
+    }
+    
+    return HttpResponse(template.render(context, request))
 
 def index(request):
     return(HttpResponse('Hello World'))
