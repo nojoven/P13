597c057a917b56b515bfc17d87b1ea4962481a51
Joseph <work.cedricjoseph@gmail.com>
2021-10-27 19:03:12 +0200

    refactor: rename view, adapt links

--- weatwork/core/urls.py
+++ weatwork/core/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls import url
 from django.urls import path
+from django.urls.conf import include
 
 from core import views
 
@@ -6,10 +8,11 @@ from core import views
 app_name = 'core'
 
 urlpatterns = [
+    url(r'^', include('django.contrib.auth.urls')),
     # /home
     path('home/', views.home, name='home'),
     
     # /home/<int>
-    path('user/<int:user_id>/', views.user, name='user'),
+    path('showcase/user/<int:user_id>/', views.showcase, name='showcase'),
     # path('me/', views.ManageUserView.as_view(), name='me'),
 ]
--- weatwork/core/views.py
+++ weatwork/core/views.py
@@ -3,17 +3,17 @@ from django.http import HttpResponse
 from .models import User
 
 def home(request):
-    users = User.objects.all().filter(is_superuser=False, is_active=True)
+    users = User.objects.all().filter(is_superuser=True, is_active=True)
     context = {
         'users_list': users,
     }
     return render(request, 'core/home.html', context)
 
 
-def user(request, user_id):
+def showcase(request, user_id):
     
     
     user = User.objects.get(pk=user_id)
     context = {'user' : user}
     
-    return render(request, 'core/user.html', context)
+    return render(request, 'core/showcase.html', context)
