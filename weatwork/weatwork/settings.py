94a41607db33cc9cf14f4228c369078846fabb4c
Joseph <work.cedricjoseph@gmail.com>
2021-10-30 22:48:32 +0200

    feat: add login url

--- weatwork/weatwork/settings.py
+++ weatwork/weatwork/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "core:home"
 LOGOUT_REDIRECT_URL = "core:home"
 
