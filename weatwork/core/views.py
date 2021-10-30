d90a022622c1578463c0c8c557b191ff07a708c1
Joseph <work.cedricjoseph@gmail.com>
2021-10-30 14:40:07 +0200

    refactor: moved showcase template in the user app

--- weatwork/core/views.py
+++ weatwork/core/views.py
@@ -13,7 +13,7 @@ def showcase(request, user_id):
     user = User.objects.get(pk=user_id)
     context = {'user' : user}
     
-    return render(request, 'core/showcase.html', context)
+    return render(request, 'user/showcase.html', context)
 
 def add_file(request):
     form = MediaForm(request.POST or None, request.FILES)
