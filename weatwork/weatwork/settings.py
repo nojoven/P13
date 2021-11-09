b3494652e7ec63f5f1aa82e6176fbe7dda067f17
Joseph <work.cedricjoseph@gmail.com>
2021-11-09 13:46:04 +0100

    refactor: add a media directory by adding media_root and media_url to settings.py

--- weatwork/weatwork/settings.py
+++ weatwork/weatwork/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = "/static/"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "core:home"
 LOGOUT_REDIRECT_URL = "core:home"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
